adf66881b9bd7b0493bc441bca99646454a2785c
zriback <zacharythecdr@gmail.com>
2025-10-13 22:09:51 -0400

    Implement SVM. Currently just did benign vs malware samples

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ class SVM:
         self.sample_hashes = sample_hashes
         self.ground_truth_str = ground_truth
         self.ground_truth_int = dict()
+        self.verbose = False
 
         # Model parameters
-        self.C = 1
+        self.C = 0.001
 
         # Trainable model values
         self.w = torch.zeros(self.feature_vectors.shape[1], dtype=torch.float32, requires_grad=True)
@@ -33,13 +34,15 @@ class SVM:
         for sample_hash, class_ in self.ground_truth_str.items():
             self.ground_truth_int[sample_hash] = class_distinguisher(class_)
 
-    def calculate_prediction(self, feature_vector: npt.NDArray[np.bool_]) -> float:
-        prediction = np.dot(self.w, feature_vector) + self.b
-        return float(prediction)
+    def calculate_prediction_torch(self, feature_vector: torch.Tensor) -> torch.Tensor:
+        prediction = torch.dot(feature_vector, self.w) + self.b
+        return prediction
 
-    def calculate_hinge_loss(self, raw_prediction: int, true_class_label: int) -> float:
-        hinge_loss = max(0, 1 - (raw_prediction*true_class_label)) + (self.C * (np.linalg.norm(self.w) / 2))
-        return float(hinge_loss)
+    def calculate_hinge_loss_torch(self, raw_prediction: torch.Tensor, true_class_label: torch.Tensor) -> torch.Tensor:
+        """Computes hinge loss and works with torch"""
+        hinge_loss = torch.clamp(1 - (raw_prediction * true_class_label), min=0)
+        hinge_loss = hinge_loss + self.C * (torch.norm(self.w) / 2)
+        return hinge_loss
     
     def evaluate(self, epochs: int, lr: float, train_test_split: float):
         # Shuffle and then do train/test split
@@ -47,18 +50,66 @@ class SVM:
         feature_vectors_shuffled = self.feature_vectors[p]
         sample_hashes_shuffled = [self.sample_hashes[i] for i in p]
 
-        index_split_value = train_test_split*feature_vectors_shuffled.shape[0]
+        index_split_value = int(train_test_split*feature_vectors_shuffled.shape[0])
         feature_vector_train = feature_vectors_shuffled[:index_split_value]
         feature_vector_test = feature_vectors_shuffled[index_split_value:]
         sample_hashes_train = sample_hashes_shuffled[:index_split_value]
         sample_hashes_test = sample_hashes_shuffled[index_split_value:]
 
-        # problem for later...
-        # TODO: gotta do the stochastic gradient decent here using torch stuff
+        # Convert everything to torch tensors
+        X_train = torch.tensor(feature_vector_train, dtype=torch.float32)
+        X_test = torch.tensor(feature_vector_test, dtype=torch.float32)
 
+        y_train = torch.tensor([self.ground_truth_int[h] for h in sample_hashes_train], dtype=torch.float32)
+        y_test = torch.tensor([self.ground_truth_int[h] for h in sample_hashes_test], dtype=torch.float32)
 
+        # Training loop
+        for epoch in range(1, epochs+1):
+            total_loss = 0
 
+            # Randomize the order of the train sets
+            p = torch.randperm(len(X_train))
+            X_train, y_train = X_train[p], y_train[p]
+            
+            # Loop over every sample in the training data
+            # For each sample, calculate hinge loss and update gradients
+            for sample, true_label in zip(X_train, y_train):
+                # Make prediction
+                y_pred = self.calculate_prediction_torch(sample)
 
+                # Calculate hinge loss
+                hinge_loss = self.calculate_hinge_loss_torch(y_pred, true_label)
+
+                # Compute gradients
+                hinge_loss.backward()
+
+                # Update model values
+                # Pylance might complain with the "type: ignore" comments
+                with torch.no_grad():
+                    self.w -= lr * self.w.grad  # type: ignore
+                    self.b -= lr * self.b.grad  # type: ignore
+                
+                self.w.grad.zero_()  # type: ignore
+                self.b.grad.zero_()  # type: ignore
+
+                total_loss += hinge_loss.item()
+
+            if self.verbose:
+                if epoch % 1 == 0:
+                    average_loss = total_loss / len(X_train)
+                    print(f'Epoch {epoch}/{epochs} - Average Loss: {average_loss:.4f}')
+        
+        # Training is done...now evaluate using the test data
+        with torch.no_grad():
+            correct_counter = 0
+            for sample, true_label in zip(X_test, y_test):
+                y_pred = self.calculate_prediction_torch(sample)
+                
+                if torch.sign(y_pred).item() == true_label.item():
+                    correct_counter += 1
+        
+        green_print(f'{correct_counter}/{len(X_test)} were classified correctly.')
+        green_print(f'Accuracy: {100*correct_counter/len(X_test):.2f}%')
 
 
 def green_print(msg: str):
@@ -141,6 +192,13 @@ def main():
     print(f'{feature_vectors.shape[1]} features per sample')
     print(f'Percent that is benign: {(benign_samples_limit/feature_vectors.shape[0])*100:.2f}%')
     print()
+
+    svm = SVM(feature_vectors, sample_hashes, ground_truth)
+    svm.define_classes(lambda x: 1 if x == 'Benign' else -1)
+    svm.verbose = True
+
+    svm.evaluate(20, 1e-4, 0.8)
+
 
 
 if __name__ == '__main__':
